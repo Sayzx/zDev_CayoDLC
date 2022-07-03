c6444904aacc148f8aefc3240af9abe9a0da2965
Sayz <74567624+Sayzx@users.noreply.github.com>
2022-07-03 12:43:14 +0200

    Patch Cayo

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+
+fx_version 'bodacious'
+game 'gta5'
+this_is_a_map 'yes'
+
+
+name "zdev_cayo"
+description "Cayo Perico DLC"
+author "Sayx"
+version "1"
+
+
+client_scripts {
+	'client/*.lua'
+}
+
